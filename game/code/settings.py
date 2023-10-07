16277b062d1b42a5e01584917b720b5f058a93b8
Alberto Romero <optimussky05@gmail.com>
2023-10-06 20:01:30 -0600

    All game to start Again route python game/main.py

---
+++ game/code/settings.py
@@ -0,0 +1,27 @@
+"""Game settings."""
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+
+LAYERS = {
+    'BG': 0,
+    'BG Detail': 1,
+    'Level': 2,
+    'FG Detail Bottom': 3,
+    'FG Detail Top': 4,
+}
+
+PATHS = {
+    'game_icon': 'game/graphics/player/right/0.png',
+    'map': 'game/data/map.tmx',
+    'player': 'game/graphics/player',
+    'bullet': 'game/graphics/bullet.png',
+    'fire_0': 'game/graphics/fire/0.png',
+    'fire_1': 'game/graphics/fire/1.png',
+    'bg_sky': 'game/graphics/sky/bg_sky.png',
+    'fg_sky': 'game/graphics/sky/fg_sky.png',
+    'enemy': 'game/graphics/enemy',
+    'health': 'game/graphics/health.png',
+    'music': 'game/audio/music.wav',
+    'hit_sound': 'game/audio/hit.wav',
+    'bullet_sound': 'game/audio/bullet.wav'
+}
---
+++ game/main.py
@@ -0,0 +1,148 @@
+"""Game main module."""
+import sys
+
+import pygame as pg
+from pytmx.util_pygame import load_pygame
+
+from code.settings import WINDOW_WIDTH, WINDOW_HEIGHT, PATHS, LAYERS
+from code.tiles import Tile, CollisionTile, MovingPlatform
+from code.player import Player
+from code.sprites import AllSprites
+from code.bullets import Bullet, FireAnimation
+from code.enemies import Enemy
+from code.overlays import HealthBar
+
+# TODO: delete enemies folder
+
+
+class Main:
+    """Game main class."""
+
+    def __init__(self):
+        """Initializes game."""
+        pg.init()
+        self.display_surface = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.icon = pg.image.load(PATHS['game_icon'])
+        pg.display.set_icon(self.icon)
+        pg.display.set_caption('JerryCombat')
+        self.clock = pg.time.Clock()
+
+        # Groups
+        self.all_sprites = AllSprites()
+        self.collision_sprites = pg.sprite.Group()
+        self.platform_sprites = pg.sprite.Group()
+        self.bullet_sprites = pg.sprite.Group()
+        self.vulnerable_sprites = pg.sprite.Group()
+
+        self.setup()
+        self.health_bar = HealthBar(self.player)
+
+        # bullet images
+        self.bullet_surf = pg.image.load(PATHS['bullet']).convert_alpha()
+        self.fire_surfs = [
+            pg.image.load(PATHS['fire_0']).convert_alpha(), pg.image.load(PATHS['fire_1']).convert_alpha()
+        ]
+
+        # music
+        self.music = pg.mixer.Sound(PATHS['music'])
+        self.music.play(loops=-1)
+        self.music.set_volume(0.5)
+
+    def setup(self):
+        """Sets up game."""
+        tmx_map = load_pygame(PATHS['map'])
+
+        # Collision tiles
+        for x, y, surf in tmx_map.get_layer_by_name('Level').tiles():
+            CollisionTile(pos=(x * 64, y * 64), surf=surf, groups=[self.all_sprites, self.collision_sprites])
+
+        for layer in ['BG', 'BG Detail', 'FG Detail Bottom', 'FG Detail Top']:
+            for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+                Tile(pos=(x * 64, y * 64), surf=surf, groups=self.all_sprites, z=LAYERS[layer])
+
+        # Objects
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            if obj.name == 'Player':
+                self.player = Player(
+                    pos=(obj.x, obj.y),
+                    groups=[self.all_sprites, self.vulnerable_sprites],
+                    path=PATHS['player'],
+                    collision_sprites=self.collision_sprites,
+                    shoot=self.shoot
+                )
+            if obj.name == 'Enemy':
+                Enemy(
+                    pos=(obj.x, obj.y),
+                    path=PATHS['enemy'],
+                    groups=[self.all_sprites, self.vulnerable_sprites],
+                    shoot=self.shoot,
+                    player=self.player,
+                    collision_sprites=self.collision_sprites
+                )
+
+        # Platforms
+        self.platform_border_rects = []
+        for obj in tmx_map.get_layer_by_name('Platforms'):
+            if obj.name == 'Platform':
+                MovingPlatform(
+                    (obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.platform_sprites]
+                )
+            else:  # border
+                border_rect = pg.Rect(obj.x, obj.y, obj.width, obj.height)
+                self.platform_border_rects.append(border_rect)
+
+    def platform_collisions(self):
+        for platform in self.platform_sprites.sprites():
+            for border in self.platform_border_rects:
+                if platform.rect.colliderect(border):
+                    if platform.direction.y < 0:  # up
+                        platform.rect.top = border.bottom
+                        platform.pos.y = platform.rect.y
+                        platform.direction.y = 1
+                    else:  # down
+                        platform.rect.bottom = border.top
+                        platform.pos.y = platform.rect.y
+                        platform.direction.y = -1
+            if platform.rect.colliderect(self.player.rect) and self.player.rect.centery > platform.rect.centery:
+                platform.rect.bottom = self.player.rect.top
+                platform.pos.y = platform.rect.y
+                platform.direction.y = -1
+
+    def bullet_collisions(self):
+        # obstacles
+        for obstacle in self.collision_sprites.sprites():
+            pg.sprite.spritecollide(obstacle, self.bullet_sprites, True)
+
+        # entities
+        for sprite in self.vulnerable_sprites.sprites():
+            if pg.sprite.spritecollide(sprite, self.bullet_sprites, True, pg.sprite.collide_mask):
+                sprite.damage()
+
+    def shoot(self, pos, direction, entity):
+        Bullet(pos, self.bullet_surf, direction, [self.all_sprites, self.bullet_sprites])
+        FireAnimation(entity, self.fire_surfs, direction, self.all_sprites)
+
+    def run(self):
+        """Game entry point."""
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+
+            self.display_surface.fill((249, 131, 103))
+
+            self.platform_collisions()
+            self.all_sprites.update(dt)
+            self.bullet_collisions()
+            self.all_sprites.custom_draw(player=self.player)
+            self.health_bar.display()
+
+            pg.display.update()
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
